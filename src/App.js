4be51618b637c7564f5084f1679937353f1503a5
mwagdi <mahmoud.elawadi@hey.car>
2021-11-03 16:41:58 +0100

    add link

--- src/App.js
+++ src/App.js
@@ -2,22 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleClick = () => {
+    window.parent.postMessage('make=vw&model=vw_amarok&trim=vw_amarok_all','*')
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClick}>Click here</button>
     </div>
   );
 }
